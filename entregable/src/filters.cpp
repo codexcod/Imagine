951dc4525c3fae9654926814bf8bc0dfb16fe920
Joaquin Nuñez <nunezjoac@gmail.com>
2022-05-27 20:58:05 -0300

    First Commit

---
+++ entregable/src/filters.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <math.h>       /* sqrt */
+#include <vector>
+#include "filters.h"
+#include <thread>  
+#include <atomic>  
+
+#define BLACK 0
+
+using namespace std;
+
+// COMPLETAR :)
+
+// Filtro plano como ejemplo
+
+void plain(ppm& img, unsigned char c)
+{
+
+	for(int i = 0; i < img.height; i++)
+		for(int j = 0; j < img.width; j++)			
+			img.setPixel(i, j, pixel(c,c,c));
+
+}
