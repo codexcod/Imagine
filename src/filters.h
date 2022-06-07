a5d1dbf809729079a87b6f1e6baeff1b97ec471a
galobponce <personal.galo.bello@gmail.com>
2022-06-07 00:59:23 +0000

    agrego filtros a realizar con firma

--- src/filters.h
+++ src/filters.h
@@ -11,17 +11,17 @@
 
 // SINGLE-THREAD FILTERS
 
-void blackWhite(ppm& img);
-void contrast(ppm& img, float contrast);
-void brightness(ppm& img, float b, int start, int end);
+void blackWhite(ppm& img, int start, int end);
 void shades(ppm& img, unsigned char shades);
 void merge(ppm& img1, ppm& img2, float alpha);
-void frame(ppm& img, pixel color, int x);
-void boxBlur(ppm &img);
-void zoom(ppm &img, ppm &img_zoomed, int n);
 void edgeDetection(ppm &img, ppm &img_target);
 void plain(ppm &img, unsigned char c);
+void zoom(ppm &img, ppm &img_zoomed, int n);
+void crop(ppm &img, int rows, int columns);
 
 // MULTI-THREAD FILTERS
+
+void blackWhiteMultiThread(ppm& img, int n_threads);
+void edgeDetectionMultiThread(ppm &img, ppm &img_target, int n_threads);
 
 #endif
