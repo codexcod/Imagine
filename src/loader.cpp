99a29a06b595f994b5ae71c7a7db3eefd33955bc
Joaquin Nuñez <nunezjoac@gmail.com>
2022-06-11 22:54:52 -0300

    agrego loader

--- src/loader.cpp
+++ src/loader.cpp
@@ -12,18 +12,26 @@ using namespace std;
 using namespace filesystem;
 
 int main(int argc , char* argv[]){
-
+	
+		cout << argc << endl;
+	
 	if(string(argv[1]) == "--help" || string(argv[1]) == "-h"){
-		cout << "Usage: ./loader <filter> <p1> <n_threads> <root_dir> <output_dir>" << endl;
+		cout << "Usage: ./loader <filtro> <n_threads> <p1> <dir> <out_dir> <p2>" << endl;
 		return 0; 
 	}
 	
 	string filter = string(argv[1]);
-	float p1 = atof(argv[2]);
-	unsigned int n = atoi(argv[3]);
+	unsigned int n_threads = atoi(argv[2]);
+	float p1 = atof(argv[3]);
 	string dir = string(argv[4]);
 	path out_dir = path(dir);
 	if(argv[5]) out_dir = path(string(argv[5]));
+	float p2;
+
+	if (argc == 7)
+	{
+		p2 = atof(argv[6]);
+	}
 
 	// Create the directory if it doesn't exist.
 	if (!is_directory(out_dir)) create_directory(out_dir);
@@ -34,15 +42,32 @@ int main(int argc , char* argv[]){
 		if (img_path.extension() != ".ppm") continue;
 
         ppm img(img_path);
-		if (filter == "plain") plain(img, (unsigned char)p1);
-		else if (filter == "blackwhite") blackWhite(img);
-		else if (filter == "shades") shades(img, (unsigned char)p1);
-		else if (filter == "contrast") contrast(img, (float)p1);
-		else if (filter == "bright") brightness(img, (float)p1);
-		else if (filter == "frame") frame(img, (int)p1);
-		else if (filter == "zoom") zoom(img, (int)p1);
-		else if (filter == "blur") boxBlur(img, p1);	
-		else if (filter == "edge") edgeDetection(img);	
+		
+		if (filter == "plain") 
+		{
+			plain(img, (unsigned char)p1);
+		}
+		else if (filter == "BYN") 
+		{
+			blackWhiteMultiThread(img, n_threads);
+		} 
+		else if (filter == "shades")
+		{
+			shades(img, p1);
+		}
+		else if (filter == "zoom")
+		{
+			ppm img_aux(img);
+			zoom(img, img_aux, p1);
+		}
+		else if (filter == "crop")
+		{
+			crop(img, p1, p2);
+		}
+		else if (filter == "ed")
+		{
+			edgeDetectionMultiThread(img, n_threads);
+		}
 	
 		path out_path = out_dir; 
 		out_path /= string(img_path.stem()) + "_" + filter + ".ppm";
